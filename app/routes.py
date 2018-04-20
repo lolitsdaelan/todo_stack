fdffdfb6bff636cf80d778e11756f3bf28c764e4
daelan.scroggins@gmail.com <daelan.scroggins@gmail.com>
2018-04-20 19:49:56 -0400

    Basic form for task entry implemented

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import NewTaskForm
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	form = NewTaskForm()
+	if form.validate_on_submit():
+		flash('You just added the following task: {}'.format(
+			form.task.data))
+		return redirect('/index')
+	return render_template('index.html', form=form)
